691c982cbf48c25b20ee2a4497f23f317305e59c
shaden <shaden.kalayed@gmail.com>
2024-03-19 12:42:27 +0300

    Menu component

--- components/Test.vue
+++ components/Test.vue
@@ -1,8 +1,15 @@
 <template>
-    <div>
-        Test component 
-        <v-btn append-icon="$vuetify">
-  Button
-</v-btn>
-    </div>
-</template>
+    <v-layout class="rounded rounded-md">
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+  
+      <v-app-bar title="Application bar"></v-app-bar>
+  
+      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+        Main Content
+      </v-main>
+    </v-layout>
+  </template>
